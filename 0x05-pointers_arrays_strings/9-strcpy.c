c53a73df2358ab803574f1284382853c8ec280ad
lmoush <zaghlouleabdelhak@gmail.com>
2023-03-27 15:32:12 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, length;
+
+	for (length = 0; src[length] != '\0'; length++)
+	{
+	}
+
+	for (i = 0; i <= length ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
