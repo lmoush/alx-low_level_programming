b7c70f878f58394f760089843e08fbdcc6ac95bc
lmoush <zaghlouleabdelhak@gmail.com>
2023-03-31 01:36:07 -0700

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,52 +1,49 @@
 #include "main.h"
-
 /**
- * base10 - power in 10 base
- * @n: an exponent
- * Return: returns 10 to power exponent
- */
-int base10(int n)
-{
-	int base = 10;
-
-	while (n > 0)
-	{
-		base *= 10;
-		n--;
-	}
-	return (base);
-}
-
-/**
- * print_number - prints integers enters as parameters using putchar
- * @n: integer to print
- * Return: void
+ *print_number - prints an integer.
+ *only using the putchar function.
+ *noarrays and pointers.
+ *@n: integer to be printed.
+ *
+ *Return: void.
  */
 void print_number(int n)
 {
-	int power;
+	int i, j, digit, digits, power;
+	unsigned int temp, numchar, number;
 
-	power = base10(8);
-
+	digit = 0;
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		temp = -n;
 	}
-
-	if (n == 0)
-		_putchar('0');
-
 	else
 	{
-		while (n / power == 0)
-			power /= 10;
+		temp = n;
+	}
 
-		while (power >= 1)
-		{
-			_putchar((n / power) + '0');
-			n %= power;
-			power /= 10;
-		}
+	number = temp;
+
+	while (number >= 10)
+	{
+		number = number / 10;
+		digit++;
+	}
+	digits = digit + 1;
+	power = 1;
+	i = 1;
+
+	while (i < digits)
+	{
+		power = power * 10;
+		i++;
+	}
+	j = power;
+	while (j >= 1)
+	{
+		numchar = (temp / j) % 10;
+		_putchar(numchar + '0');
+		j = j / 10;
 	}
 }
