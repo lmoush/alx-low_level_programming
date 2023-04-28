673f513c60e264b2748a0ea1cfa60f3e838a8c40
lmoush <zaghlouleabdelhak@gmail.com>
2023-04-27 17:01:31 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,10 +21,10 @@ list_t *add_node(list_t **head, const char *str)
 	if (!new)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = (*head);
-	(*head)= new; 
+new->str = strdup(str);
+new->len = len;
+new->next = (*head);
+(*head) = new; 
 
 	return (*head);
 }
