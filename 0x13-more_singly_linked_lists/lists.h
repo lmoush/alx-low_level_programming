b234f04822ba0b7f21e73927d586ab274f5fbb2b
lmoush <zaghlouleabdelhak@gmail.com>
2023-05-01 20:37:59 -0700

    green cat

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,18 +1,25 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>  // For printf
-#include <stdlib.h> // For malloc, free
-#include <string.h> // For size_t
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* malloc, free */
+#include <string.h> /* size_t */
 
-// Structure for a singly linked list node
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-// Function prototypes
+/* function prototypes */
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
