faeb90babecd8136343cc3d164c443afd8ab11dc
lmoush <zaghlouleabdelhak@gmail.com>
2023-05-01 20:58:26 -0700

    green cat

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current_node = h;
-    size_t node_count = 0;
+	const listint_t *current_node = h;
+	size_t node_count = 0;
 
-    while (current_node != NULL)
-    {
-        printf("%d\n", current_node->n);
-        node_count++;
-        current_node = current_node->next;
-    }
+	while (current_node != NULL)
+	{
+		printf("%d\n", current_node->n);
+		node_count++;
+		current_node = current_node->next;
+	}
 
-    return node_count;
+	return (node_count);
 }
