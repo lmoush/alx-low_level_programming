6e8f24e896a683e03bae97baea5e09ddec7d4100
lmoush <zaghlouleabdelhak@gmail.com>
2023-05-04 15:55:47 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/_putchar.c
+++ 0x14-bit_manipulation/_putchar.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character d to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char d)
+int _putchar(char c)
 {
-	return (write(1, &d, 1));
+	return (write(1, &c, 1));
 }
