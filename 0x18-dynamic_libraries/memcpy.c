81a387c75c80497463c87ffbcf6dcf306f11aa93
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-19 19:19:42 -0700

    lmoush

---
+++ 0x18-dynamic_libraries/memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * *_memcpy - function fyhufyf
+ * @dest: This is a pointer to jdjdj
+ * @src: This is a pointer khjkhk
+ * @n: number of bytes to hughui
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
