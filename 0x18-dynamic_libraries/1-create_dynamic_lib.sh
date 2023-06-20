09169e2e669152de8ba5162c36c46a76b5ed91c9
lmoush <zaghlouleabdelhak@gmail.com>
2023-06-19 19:23:09 -0700

    lmoush

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#!/bin/bash
-gcc -c -Wall -Werror -fpic *.c
-gcc -shared -o liball.so *.o
+gcc -fPIC -g -c -Wall -pedantic -Werror -Wextra *.c
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o -lc
